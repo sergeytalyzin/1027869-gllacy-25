43ef6dc03fd43e21c4e093c1bee64f13911bcb98
sergey <50322776+sergeytalyzin@users.noreply.github.com>
2019-07-06 21:33:22 +0300

    csdasd

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var feedback = document.querySelector(".button-feedback");
+var popup = document.querySelector(".modal-feedback-open");
+var close = document.querySelector(".feedback-close");
+
+
+feedback.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+})
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+})
