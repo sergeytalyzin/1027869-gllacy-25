3226511e4c01ca9e7f27c604a184f0185b2c310e
sergey <50322776+sergeytalyzin@users.noreply.github.com>
2019-07-08 12:45:26 +0300

    888

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var feedback = document.querySelector(".button-feedback");
 var popup = document.querySelector(".modal-feedback-open");
 var close = document.querySelector(".feedback-close");
-
+var login = document.querySelector(".nick");
+var form = document.querySelector(".modal-feedback");
+var email = document.querySelector(".mail")
+var textarea = document.querySelector(".textarea")
 
 feedback.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -11,3 +14,31 @@ close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 })
+
+form.addEventListener("sudmit" function(evt) {
+  if (!login.value || !email.value || !textarea) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.class.add("modal-error");
+
+    if (!login.value) {
+      login.classList.add("input-error");
+    } else {
+      login.classList.remove("input-error");
+    }
+    if (!email.value) {
+      email.classList.add("input-error");
+    } else {
+      email.classList.remove("input-error");
+    }
+    if (!textarea.value) {
+      textarea.classList.add("input-error");
+    } else {
+      textarea.classList.remove("input-error");
+    }
+
+  }
+
+});
+
